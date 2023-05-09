5dc38a5e3297ed6ba776fe01ba67e072d91a55ed
isacancabuk <isacancabuk@gmail.com>
2023-05-09 12:29:31 +0300

    Allready created and prepered files are added.

---
+++ script.js
@@ -0,0 +1,30 @@
+// name -> base_url/search/batman
+// id -> base_url/id
+
+const randomHero = document.getElementById('randomHero')
+const findHero = document.getElementById('findHero')
+const input = document.getElementById('input')
+const footer = document.querySelector('footer')
+
+const randomNumber = () => {
+  const number = Math.floor(Math.random() * 731) + 1
+  return number
+}
+
+const getRandom = number => {
+  fetch(`https://www.superheroapi.com/api.php/6276633832387040/${number}`)
+    .then(response => response.json())
+    .then(json => (footer.innerHTML = `<img src="${json.image.url}"/>`))
+}
+
+const getHero = () => {
+  // console.log('birsey')
+  fetch(`https://www.superheroapi.com/api.php/6276633832387040/search/${input.value}`)
+    .then(response => response.json())
+    .then(json => (footer.innerHTML = `<img src="${json.results[0].image.url}"/>`))
+}
+
+getRandom(randomNumber())
+getHero()
+
+randomHero.onclick = () => getRandom(randomNumber())
